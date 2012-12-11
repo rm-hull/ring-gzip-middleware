ffcf87ae6b04d2d90563709c3b2beb2f0f18a841
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-11 22:17:07 +0000

    Updated scm, pom-additions, license, etc, etc

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject amalloy/ring-gzip-middleware "0.1.2"
+(defproject rm-hull/ring-gzip-middleware "0.1.4-SNAPSHOT"
   :description "Ring gzip encoding middleware"
+  :url "https://github.com/rm_hull/ring-gzip-middleware"
+  :license {:name "Distributed under an MIT-style license (see LICENSE for details)." }
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:1.2.0 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-             :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4.0 {}}
-  :aliases {"all" ["with-profile" "1.2.0:1.3.0:1.4.0"]}
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :warn-on-reflection true
+  :scm {:url "git@github.com:rm-hull/ring-gzip-middleware.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Richard Hull"]
+                              [:url "http://destructuring-bind.org"]
+                              [:email "richard.hull@destructuring-bind.org"]
+                              [:timezone "0"]]])
