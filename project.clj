bac97f96a19e1197352e1ba745044007dd078ba4
Richard Hull <rm_hull@yahoo.co.uk>
2013-01-25 00:16:26 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Ring gzip encoding middleware"
   :url "https://github.com/rm_hull/ring-gzip-middleware"
   :license {:name "Distributed under an MIT-style license (see LICENSE for details)." }
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0-RC2"]]
   :min-lein-version "2.0.0"
   :warn-on-reflection true
   :scm {:url "git@github.com:rm-hull/ring-gzip-middleware.git"}
