f7dc75d79cb0f32e58e59257767bc67689d051ac
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-23 18:36:48 +0000

    No longer snapshot

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject rm-hull/ring-gzip-middleware "0.1.5-SNAPSHOT"
+(defproject rm-hull/ring-gzip-middleware "0.1.5"
   :description "Ring gzip encoding middleware"
   :url "https://github.com/rm_hull/ring-gzip-middleware"
   :license {:name "Distributed under an MIT-style license (see LICENSE for details)." }
-  :dependencies [[org.clojure/clojure "1.5.0"]]
-  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :min-lein-version "2.1.1"
   :warn-on-reflection true
   :scm {:url "git@github.com:rm-hull/ring-gzip-middleware.git"}
   :pom-addition [:developers [:developer
